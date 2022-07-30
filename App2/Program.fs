5140e017c2a6bd4b8881710e10a400fea89faea6
hamy <hamy@hamy.xyz>
2022-07-30 14:02:59 -0400

    [App2] Add cors support

--- App2/Program.fs
+++ App2/Program.fs
@@ -1,5 +1,6 @@
 open System
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Cors
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
@@ -11,11 +12,21 @@ let webApp =
         route "/"       >=> htmlFile "/pages/index.html" ]
 
 let configureApp (app : IApplicationBuilder) =
+    app.UseCors(
+        Action<_>
+            (fun (b: Infrastructure.CorsPolicyBuilder) ->
+                // Put real allowed origins in here
+                b.AllowAnyHeader() |> ignore
+                b.AllowAnyMethod() |> ignore
+                b.AllowAnyOrigin() |> ignore)
+    )
+    |> ignore
+
     // Add Giraffe to the ASP.NET Core pipeline
     app.UseGiraffe webApp
 
 let configureServices (services : IServiceCollection) =
-    
+    services.AddCors() |> ignore
 
     // Add Giraffe dependencies
     services.AddGiraffe() |> ignore
